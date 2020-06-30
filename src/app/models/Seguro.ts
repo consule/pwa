24b39bd2670f333bc5d831311fed1bd2df63afab
Cristiano <cristianoconsule@gmail.com>
2020-06-30 20:18:38 -0300

    Layout e Consumo de API externa jsonp

---
+++ src/app/models/Seguro.ts
@@ -0,0 +1,10 @@
+import { MarcaCarro } from './MarcaCarros';
+
+export class Seguro {
+    marcaCarro: MarcaCarro;
+    modeloCarro: string;
+    placaCarro: string;
+    nomeProprietario: string;
+    sobrenomeProprietario: string;
+    dataNascimentoProprietario: string;
+}
